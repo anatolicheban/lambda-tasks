d615f01e0b49aab0c0c385f49136c3d7b5df0df8
anatolicheban <107035461+anatolicheban@users.noreply.github.com>
2022-11-01 20:33:23 +0200

    telegram-console-sender completed

---
+++ telegram-console-sender/sender.js
@@ -0,0 +1,33 @@
+import { users, token } from './server.js';
+import { Command } from 'commander';
+import TelegramBot from 'node-telegram-bot-api';
+const program = new Command();
+
+
+const bot = new TelegramBot(token, { polling: false })
+
+program
+  .name('sender')
+  .description('CLI to send meessages and photos from telegram bot')
+  .version('0.0.1');
+
+program.command('message')
+  .description('Sends a text message to users')
+  .argument('<string>')
+  .action((msg) => {
+    users.forEach(user => {
+      bot.sendMessage(user.id, msg)
+    })
+  });
+
+program.command('photo')
+  .description('Sends a photo to users')
+  .argument('<string>')
+  .action((msg) => {
+    users.forEach(user => {
+      bot.sendPhoto(user.id, msg)
+    })
+  });
+
+
+program.parse();
---
+++ telegram-console-sender/server.js
@@ -0,0 +1,27 @@
+import TelegramBot from 'node-telegram-bot-api'
+import { writeFileSync, readFileSync } from "fs";
+import lodash from 'lodash'
+
+//Получаем простенькую бд
+export const users = JSON.parse(readFileSync('users.json', 'utf-8'))
+
+// Подключаемся к боту
+export const token = '5684482539:AAG0L1waRyb3URkWgyc051MLRVdPcPigV3A'
+const bot = new TelegramBot(token, { polling: true })
+
+//Вносим пользователя в бд при команде /start
+bot.onText(/\/start/, (msg) => {
+  const user = {
+    username: msg.chat.username,
+    id: msg.chat.id,
+  }
+  //Проверяем есть ли такой пользователь в бд
+  let isInDatabase = users.find(item => lodash.isEqual(item, user))
+  if (!isInDatabase) {
+    users.push(user)
+    //Перезаписываем бд
+    writeFileSync('users.json', JSON.stringify(users))
+    bot.sendMessage(msg.chat.id, 'Ok, wait for the messages');
+  }
+
+});
